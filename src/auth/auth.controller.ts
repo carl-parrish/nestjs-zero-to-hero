538d4d25489a356911a4355f1ecba26198e551a7
Carl Parrish <carl.parrish@gmail.com>
2024-01-30 10:03:10 -0700

    docs: clean up formating

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('/signup')
+  signUp(
+    @Body()
+    authCredentials: Prisma.UserCreateInput,
+  ): Promise<User> {
+    return this.authService.signUp(authCredentials);
+  }
+}
