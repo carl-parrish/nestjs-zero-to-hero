665f859fca6260d5ab0a135aacebafd0206495a7
Carl Parrish <carl.parrish@gmail.com>
2024-01-30 09:46:16 -0700

    fix: database changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
-
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [TasksModule, AuthModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
